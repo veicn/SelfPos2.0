15d24fa081afc83147e015b702f128e597f16e0e
xiezhuolin <xiezhuolin2011@163.com>
2018-12-07 11:38:43 +0800

    [重装电脑之后提交一次代码]

--- app/src/main/java/com/acewill/slefpos/orderui/main/contract/MainContract.java
+++ app/src/main/java/com/acewill/slefpos/orderui/main/contract/MainContract.java
@@ -24,9 +24,6 @@ public interface MainContract {
 
 	interface View extends BaseView {
 		void returnBindResult(BindEntity bindEntity);
-
-
-
 		void returnLoginResult(LoginEntity loginEntity);
 	}
 
